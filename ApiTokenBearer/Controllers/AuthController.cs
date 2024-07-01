2f21ee0e6d06b13442acee5e577a95a3981f0e03
matthewsrsampaio <matthews_venom@hotmail.com>
2024-07-01 12:43:54 -0300

    First commit

---
+++ ApiTokenBearer/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ApiTokenBearer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("/login")]
+        public IActionResult Login(string username, string password)
+        {
+            if (IsvalidUser(username, password))
+                {
+                    var claimsPrincipal = new ClaimsPrincipal(
+                    new ClaimsIdentity(
+                        new[] { new Claim(ClaimTypes.Name, username) },
+                        BearerTokenDefaults.AuthenticationScheme
+                        )
+                    );
+                    return SignIn(claimsPrincipal);
+                }
+            return Unauthorized("Credenciais inválidas");
+        }
+
+        private bool IsvalidUser(string username, string password)
+        {
+            return username == "Matthews" && password == "123456";
+        }
+
+        [HttpGet("/user")]
+        [Authorize]
+        public IActionResult GetUser()
+        {
+            var user = User;
+            if(user?.Identity?.IsAuthenticated ?? false)
+            {
+                return Ok($"Bem-vindo {user.Identity.Name}!");
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ ApiTokenBearer/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthentication().AddBearerToken();
+builder.Services.AddAuthorization();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apicatalogo", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Bearer JWT",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
